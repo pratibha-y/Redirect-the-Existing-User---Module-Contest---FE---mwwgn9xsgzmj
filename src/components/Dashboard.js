20b8241476a8021313a5b8a5e83bc4c7041845e8
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-16 11:05:45 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/*import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 
 const Dashboard = () => {
@@ -8,6 +8,33 @@ const Dashboard = () => {
     <div className="dashboard">
       <h1>Welcome to the Dashboard</h1>
       <button className="logout-btn">Logout</button>
+    </div>
+  );
+};
+
+export default Dashboard;*/
+
+import React, { useState } from "react";
+
+import { Redirect } from "react-router-dom";
+
+const Dashboard = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  if (!isLoggedIn) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div className="dashboard">
+      <h1>Welcome to the Dashboard</h1>
+      <button className="logout-btn" onClick={handleLogout}>
+        Logout
+      </button>
     </div>
   );
 };
