cef73c176e0b1572370503bf27961a3629294d4c
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-16 11:02:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import '../styles/App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginForm from './LoginForm';
@@ -16,5 +16,29 @@ const App = () => {
   )
 }
 
+
+export default App;*/
+
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import Dashboard from "./Dashboard";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LoginForm />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
